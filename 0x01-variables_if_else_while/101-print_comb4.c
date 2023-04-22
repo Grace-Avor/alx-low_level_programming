eb9e103504a4e3498da122059c2a26bd5955c654
Grace-Avor <graceavor2021@gmail.com>
2023-04-21 22:14:11 -0700

    task 11 corr

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,22 +7,24 @@ int main(void)
 {
 	int n, m, I;
 
-	for (m = 48; m < 58; m++)
+	for (n = 48; n < 58; n++)
 
 		for (I = 50; I < 58; I++)
 
-			if (I > m && m > n)
-			{
-				putchar(n);
-				putchar(m);
-				putchar(I);
+			for (m = 49; m < 58; m++)
 
-				if (n != 55 || m != 56)
+				if (I > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(I);
+
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-			}
 	putchar('\n');
 	return (0);
 }
